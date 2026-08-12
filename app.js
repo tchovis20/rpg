2502f160db944f5467275bbcc06febb230b15bb2
tchovis20 <enzocueio123@gmail.com>
2026-08-12 09:22:34 -0300

    usamos a classe

--- app.js
+++ app.js
@@ -1,38 +1,30 @@
-    const guerreiro = {
-    nome: "Thorin",
-    vida: 60,
-    ataque: 20,
+class Personagem {
+    constructor (nome, vida, ataque){
+        this.nome = nome,
+        this.vida = vida,
+        this.ataque = ataque
+    }
 
-    causaDano: function(personagem){
-    personagem.recebeDano(this.ataque)
-    },
-    recebeDano: function(quantidade){
-    this.vida = this.vida - quantidade
-}
-  
-}
-    const mago = {
-    nome: "Gandalfe",
-    vida: 60,
-    ataque: 35,
-    recebeDano: function(quantidade){
-    this.vida = this.vida - quantidade
-    },
-causaDano: function(personagem){
-    personagem.recebeDano(this.ataque)
+    causaDano(personagem){
+        personagem.recebeDano(this.ataque)
+    }
+
+    recebeDano(quantidade){
+        this.vida = this.vida - quantidade
+        
+        if(this.vida <= 0){
+        this.vida = "eliminado"
+    }
     }
 }
-    const arqueiro = {
-    nome: "Legolas",
-    vida: 80,
-    ataque: 25,
 
-    recebeDano: function(quantidade){
-    this.vida = this.vida - quantidade
-    },
-    causaDano: function(personagem){
-    personagem.recebeDano(this.ataque)
-    }
-}
-guerreiro.causaDano(mago)
+const druida = new Personagem("Kode", 80, 25)
+
+const guerreiro = new Personagem("Thorin", 60, 20)
+
+const mago = new Personagem("Gandalfe", 60, 35)
+
+const arqueiro = new Personagem("Legolas", 80, 25)
+
+druida.causaDano(mago)
 console.log(mago.vida)
