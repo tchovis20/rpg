2896f9867116a90e2702af08d46840d0832724fb
tchovis20 <enzocueio123@gmail.com>
2026-08-13 09:21:53 -0300

    rpg com mais personagens

--- app.js
+++ app.js
@@ -19,12 +19,23 @@ class Personagem {
 }
 
 const druida = new Personagem("Kode", 80, 25)
-
 const guerreiro = new Personagem("Thorin", 60, 20)
-
 const mago = new Personagem("Gandalfe", 60, 35)
-
 const arqueiro = new Personagem("Legolas", 80, 25)
 
-druida.causaDano(mago)
-console.log(mago.vida)
+const cavaleiro = new Personagem("Cadmus", 70, 25)
+const renegado = new Personagem("Valtair", 60, 20)
+const tanque = new Personagem("Isati", 90, 15)
+
+
+
+
+druida.causaDano(guerreiro)
+guerreiro.causaDano(mago)
+mago.causaDano(arqueiro)
+arqueiro.causaDano(cavaleiro)
+cavaleiro.causaDano(renegado)
+renegado.causaDano(tanque)
+tanque.causaDano(druida)
+
+console.log(druida.vida, guerreiro.vida, mago.vida, arqueiro.vida, cavaleiro.vida, renegado.vida, tanque.vida)
