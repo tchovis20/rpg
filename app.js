fcf62277e11f558b3a00d4d86b554ea37e1877c3
tchovis20 <enzocueio123@gmail.com>
2026-09-14 08:42:52 -0300

    rpg

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 class Personagem {
-    constructor (nome, vida, ataque){
+    constructor (nome, vida, ataque, defesa){
         this.nome = nome,
         this.vida = vida,
         this.ataque = ataque
+        this.defesa = defesa
     }
 
     causaDano(personagem){
@@ -26,15 +27,34 @@ class Personagem {
     }
 }
 
-const druida = new Personagem("Kode", 80, 25)
-const guerreiro = new Personagem("Thorin", 60, 20)
-const mago = new Personagem("Gandalfe", 60, 35)
-const arqueiro = new Personagem("Legolas", 80, 25)
+const druida = new Personagem("Kode", 80, 25, 15)
+const guerreiro = new Personagem("Thorin", 60, 20, 30)
+const mago = new Personagem("Gandalfe", 60, 35, 20)
+const arqueiro = new Personagem("Legolas", 80, 25, 10)
 
-const samurai = new Personagem("Akuma", 70, 25)
-const necromante = new Personagem("Vult", 60, 20)
-const tanque = new Personagem("Isati", 90, 15)
+const samurai = new Personagem("Akuma", 70, 25, 10)
+const necromante = new Personagem("Vult", 60, 20, 20)
+const tanque = new Personagem("Isati", 90, 15, 50)
 
-druida.causaDano(mago)
-console.log(mago.estaVivo())
-console.log(mago.mostrarStatus())
+class Teste extends Personagem {
+    constructor(nome){
+        super(nome, 80, 35, 15)
+    }
+}
+
+const Sigmund = new Teste("Sigmund")
+
+const personagens = [
+    druida,
+    guerreiro,
+    mago,
+    arqueiro,
+    samurai,
+    necromante,
+    tanque,
+    Sigmund
+]
+
+personagens.forEach(function(personagem){
+    personagem.mostrarStatus()
+})
